57d7dcbe99c2366ba1d471cc33120946a6134f60
Paulo Montoya <paulo@paulomontoya.com.br>
2017-07-30 23:32:39 -0300

    Adicionado rubocop e adaptado o código.

--- nama_test.rb
+++ nama_test.rb
@@ -1,51 +1,50 @@
-=begin
+# Logica:
+#
+#  Numeros de 1 a 100.
+#
+#  Para pares, trocar numero por "Go"
+#  Para primos, trocar numero por "Nama"
+#  Para pares e primos, trocar numero por "Go Nama"
+require 'prime'
 
-  # Lógica:
+# Classe que vai carregar o teste
+class NamaTest
+  # Nesse exemplo vou usar [0..100] sempre
+  # mas coloquei para receber essa informacao :)
+  attr_accessor :array_of_numbers
 
-    Números de 1 a 100.
+  def filter_rest
+    unless @array_of_numbers.is_a?(Array)
+      raise ArgumentError, 'Os números precisam estar em um array', caller
+    end
 
-    Para pares, trocar número por "Go"
-    Para primos, trocar número por "Nama"
-    Para pares e primos, trocar número por "Go Nama"
+    @array_of_numbers.each_with_index do |actual_number, key|
+      actual_number_is_prime = Prime.prime?(actual_number)
 
-=end
-  
-  require 'prime'
-
-  class NamaTest
-
-    attr_accessor :array_of_numbers # Nesse exemplo vou usar [0..100] sempre mas coloquei para receber essa informação :)
-
-    def filter_rest
-      unless @array_of_numbers.is_a?(Array)
-        raise ArgumentError, "Os números precisam estar em um array", caller
+      if actual_number.even? && actual_number_is_prime
+        @array_of_numbers[key] = 'Go Nama'
+      elsif actual_number.even?
+        @array_of_numbers[key] = 'Go'
+      elsif actual_number_is_prime
+        @array_of_numbers[key] = 'Nama'
       end
+    end # each of @array_of_numbers
+  end # def filter_rest
 
-      @array_of_numbers.each_with_index do |actual_number, key|
+  def print_final_result
+    filter_rest
+    @array_of_numbers.join(', ')
+  end # def print_final_result
+end # NamaTest Class
 
-        actual_number_is_prime = Prime.prime?(actual_number)
+# Carrega a classe
+result_of_the_test = NamaTest.new
 
-        if actual_number % 2 == 0 && actual_number_is_prime
-          @array_of_numbers[key] = "Go Nama"
-        elsif actual_number % 2 == 0
-          @array_of_numbers[key] = "Go"
-        elsif actual_number_is_prime
-          @array_of_numbers[key] = "Nama"
-        end
+# Define os numeros a serem tratados pelo filtro
+result_of_the_test.array_of_numbers = Array(1..100)
 
-      end # each of @array_of_numbers
+# Cospe o resultado
+puts result_of_the_test.print_final_result
 
-    end # def filter_rest
-
-
-    def print_final_result
-      self.filter_rest
-      return @array_of_numbers.join(", ")
-    end # def print_final_result
-
-  end # NamaTest Class
-
-  result_of_the_test = NamaTest.new
-  result_of_the_test.array_of_numbers = Array(1..100) # Define os números a serem tratados pelo filtro
-  puts result_of_the_test.print_final_result # Cospe o resultado
-  puts "\n"
+# Adiciona uma linha final
+puts "\n"
